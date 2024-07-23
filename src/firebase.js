dc89c19891eaff7f8a3a01f817611e04224de814
busangangster <wlwhssla1034@naver.com>
2024-07-23 22:45:43 +0900

    NavBar Finished

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlpWEtXb0eCNFrDAcs5pc0Q7uSaerJjjc",
+  authDomain: "react-poke-app-f4dfc.firebaseapp.com",
+  projectId: "react-poke-app-f4dfc",
+  storageBucket: "react-poke-app-f4dfc.appspot.com",
+  messagingSenderId: "810426985019",
+  appId: "1:810426985019:web:c3fc0982676e11ffe10a1e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
